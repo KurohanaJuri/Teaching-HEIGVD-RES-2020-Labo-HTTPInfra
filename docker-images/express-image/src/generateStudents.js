83d908ea42a3ecc3d59d76685e9603fee949b652
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-05-01 15:50:30 +0200

    Add students generator

---
+++ docker-images/express-image/src/generateStudents.js
@@ -0,0 +1,32 @@
+var Chance = require('chance');
+var chance = new Chance();
+
+// Generate random students
+exports.generateStudents = function(){
+    var numberOfStudents = chance.integer({min : 0, max : 10});
+
+    console.log(numberOfStudents);
+
+    var students = [];
+
+    for(var i = 0; i < numberOfStudents; i++){
+        var gender = chance.gender();
+        var birthYear = chance.year({
+            min : 1986,
+            max : 1996
+        });
+
+        students.push({
+            firstName : chance.first({gender:gender}),
+            lastNmae : chance.last,
+            gender : gender,
+            birthday : chance.birthday({
+                year : birthYear
+            })
+        });
+    };
+
+    console.log(students);
+    return students;
+}
+
--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,15 +1,15 @@
-var Chance = require('chance');
-var chance = new Chance();
-
 var express = require('express');
 var app = express();
+
+var gs = require('./generateStudents')
 
 
 // If GET and tager resources is a '/', we execute the callback
 app.get('/', function(req, res){
-    res.send("Hello");
+    res.send(gs.generateStudents());
 });
 
 app.listen(3000, function(){
     console.log('Acception resuqueste on port 3000');
 });
+
