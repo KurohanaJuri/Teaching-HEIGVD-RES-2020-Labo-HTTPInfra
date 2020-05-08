cc28a0639d5011ad1545c8d5c9a2905e19106582
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-05-08 14:23:34 +0200

    Add comments

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,11 +4,12 @@ var app = express();
 var gs = require('./randomGenerator.js')
 
 
-// If GET and tager resources is a '/', we execute the callback
+// If GET and taget resources is a '/', we execute the callback
 app.get('/', function(req, res){
     res.send(gs.generateRandomArray(5,1,50));
 });
 
+// Listen at port 3000
 app.listen(3000, function(){
     console.log('Acception resuqueste on port 3000');
 });
