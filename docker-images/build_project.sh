b0f4ab8a0f3ddc4fa7e8c47d4d34f9ff59a00f59
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-05-23 21:19:51 +0200

    Update run_project script

--- docker-images/build_project.sh
+++ docker-images/build_project.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Apache
 docker build -t res/apache_php apache-php-image
 
--- docker-images/run_project.sh
+++ docker-images/run_project.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
 #Apache 
 docker run -d --name apache_static res/apache_php
 
 #Dynacmic server
 docker run -d --name express_dynamic res/express_students
 
+ip_static=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}"  apache_static )
+echo $ip_static
+
+
+ip_dynamic=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}"  express_dynamic )
+echo $ip_dynamic
+
 #Reverse proxy
-docker run -d --name reverse_proxy -p 8080:80 res/apache_rp
+docker run -d -e STATIC_APP="$ip_static:80" -e DYNAMIC_APP="$ip_dynamic:3000" --name reverse_proxy -p 8080:80 res/apache_rp
