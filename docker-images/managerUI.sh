398358c30513364df04b2ed7176eb89d7561d836
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-06-01 13:44:20 +0200

    fix mangerUi script

--- docker-images/managerUI.sh
+++ docker-images/managerUI.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 docker volume create portainer_data
-ocker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
